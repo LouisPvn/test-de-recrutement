0689ce930ba041f485a54b695d8edb0836fe6f63
LouisPvn <louis.pav@hotmail.fr>
2023-05-25 18:11:17 +0200

    [Finished] JS

--- js/shadow-variable.js
+++ js/shadow-variable.js
@@ -1,17 +1,11 @@
 const articleList = []; // In a real app this list would be full of articles.
 var kudos = 5;
 
-function calculateTotalKudos(articles) {
-  var kudos = 0;
-  
-  for (let article of articles) {
-    kudos += article.kudos;
-  }
-  
-  return kudos;
+function calculateTotalKudos() {
+  return articleList.reduce((tmp, article) => tmp + article.kudos, 0);
 }
 
 document.write(`
   <p>Maximum kudos you can give to an article: ${kudos}</p>
-  <p>Total Kudos already given across all articles: ${calculateTotalKudos(articleList)}</p>
+  <p>Total Kudos already given across all articles: ${calculateTotalKudos()}</p>
 `);
